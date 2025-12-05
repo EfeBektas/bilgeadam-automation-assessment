eab4fa048bbac611fb554b51f9890756ac86ae59
EfeBektas <126690737+EfeBektas@users.noreply.github.com>
2025-12-05 17:53:38 +0300

    core of the project added with driverfactory and custom exception classes

---
+++ TestAutomation/Core/Exceptions/WaitTimeoutException.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAutomation.Core.Exceptions
+{
+    public class WaitTimeoutException : Exception
+    {
+        public WaitTimeoutException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ TestAutomation/Drivers/DriverFactory.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestAutomation.Drivers
+{
+    public static class DriverFactory
+    {
+        public static IWebDriver CreateDriver(string browser)
+        {
+            IWebDriver driver;
+
+            switch (browser.ToLower())
+            {
+                case "chrome":
+                    new DriverManager().SetUpDriver(new ChromeConfig());
+                    var options = new ChromeOptions();
+                    options.AddArgument("--start-maximized");
+                    driver = new ChromeDriver(options);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unsupported browser: {browser}");
+            }
+
+            return driver;
+        }
+    }
+}

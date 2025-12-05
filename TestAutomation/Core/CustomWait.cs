1a4ffa9dbe08c331ea8be0d03061e12c87aed240
EfeBektas <126690737+EfeBektas@users.noreply.github.com>
2025-12-05 17:57:50 +0300

    added custom wait logic

---
+++ TestAutomation/Core/CustomWait.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using TestAutomation.Core.Exceptions;
+
+namespace TestAutomation.Core
+{
+    public class CustomWait
+    {
+        private readonly IWebDriver _driver;
+        private readonly int _timeoutSeconds;
+
+        public CustomWait(IWebDriver driver)
+        {
+            _driver = driver;
+            _timeoutSeconds = ConfigManager.ExplicitWait;
+        }
+
+        public IWebElement UntilVisible(By locator)
+        {
+            var endTime = DateTime.Now.AddSeconds(_timeoutSeconds);
+            while (DateTime.Now < endTime)
+            {
+                try
+                {
+                    var element = _driver.FindElement(locator);
+                    if (element.Displayed)
+                        return element;
+                }
+                catch { }
+                Thread.Sleep(200);
+            }
+            throw new WaitTimeoutException($"Element not visible after {_timeoutSeconds} seconds: {locator}");
+        }
+
+        public IWebElement UntilClickable(By locator)
+        {
+            var endTime = DateTime.Now.AddSeconds(_timeoutSeconds);
+            while (DateTime.Now < endTime)
+            {
+                try
+                {
+                    var element = _driver.FindElement(locator);
+                    if (element.Displayed && element.Enabled)
+                        return element;
+                }
+                catch { }
+                Thread.Sleep(200);
+            }
+            throw new WaitTimeoutException($"Element not clickable after {_timeoutSeconds} seconds: {locator}");
+        }
+
+        public bool UntilExists(By locator)
+        {
+            var endTime = DateTime.Now.AddSeconds(_timeoutSeconds);
+            while (DateTime.Now < endTime)
+            {
+                try
+                {
+                    _driver.FindElement(locator);
+                    return true;
+                }
+                catch { }
+                Thread.Sleep(200);
+            }
+            throw new WaitTimeoutException($"Element not found in DOM after {_timeoutSeconds} seconds: {locator}");
+        }
+    }
+}
